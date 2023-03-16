c0b9dee04f03708ff89109080faa9f1c0c2341e0
VeereshR-Wellsky <95695315+VeereshR-Wellsky@users.noreply.github.com>
2023-03-16 14:27:13 +0530

    Added Angular Application

---
+++ EmployeeAPI/EmployeeCRUD/src/app/Service/http-provider.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WebApiService } from './web-api.service';
+
+var apiUrl = "http://localhost:5066/";
+
+var httpLink = {
+  getAllEmployee: apiUrl + "/api/employee",
+  deleteEmployeeById: apiUrl + "/api/employee",
+  getEmployeeDetailById: apiUrl + "/api/employee",
+  saveEmployee: apiUrl + "/api/employee"
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpProviderService {
+  constructor(private webApiService: WebApiService) { }
+
+  public getAllEmployee(): Observable<any> {
+    return this.webApiService.get(httpLink.getAllEmployee);
+  }
+  public deleteEmployeeById(model: any): Observable<any> {
+    return this.webApiService.post(httpLink.deleteEmployeeById + '?Id=' + model, "");
+  }
+  public getEmployeeDetailById(model: any): Observable<any> {
+    return this.webApiService.get(httpLink.getEmployeeDetailById + '?Id=' + model);
+  }
+  public saveEmployee(model: any): Observable<any> {
+    return this.webApiService.post(httpLink.saveEmployee, model);
+  }  
+}
